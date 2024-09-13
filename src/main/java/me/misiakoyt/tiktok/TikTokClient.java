c6586047eed28a0d34872cca00cbd44e7cdf8374
Krzysztof Miller <misiakoyt.2008@gmail.com>
2024-09-13 11:13:00 +0200

    Initial commit

---
+++ src/main/java/me/misiakoyt/tiktok/TikTokClient.java
@@ -0,0 +1,10 @@
+package me.misiakoyt.tiktok;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TikTokClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/me/misiakoyt/tiktok/TikTokDataGenerator.java
@@ -0,0 +1,11 @@
+package me.misiakoyt.tiktok;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TikTokDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
